311a428e1166bbda76470ce1e0846643dff28128
aransandya <90309210+aransandya@users.noreply.github.com>
2021-11-26 09:42:39 +0700

    feat: add RoleModel.java and PegawaiModel.java (#5)
    Co-authored-by: Aransandya Pangesthika <aransandya@yahoo.com>

--- sifactory/src/main/java/apap/TugasAkhir/siFactory/model/PegawaiModel.java
+++ sifactory/src/main/java/apap/TugasAkhir/siFactory/model/PegawaiModel.java
@@ -1,5 +1,72 @@
 package apap.TugasAkhir.siFactory.model;
 
-public class PegawaiModel {
-    
+import apap.TugasAkhir.siFactory.model.RoleModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter @Getter
+@Entity
+@Table(name = "pegawai")
+public class PegawaiModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPegawai;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "nama", nullable = false)
+    private String nama;
+
+    @NotNull
+    @Column(name = "tanggal_lahir", nullable = false)
+    private LocalDateTime tanggalLahir;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotNull
+    @Column(name = "counter", nullable = false)
+    private Integer counter;
+
+    @NotNull
+    @Lob
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    //Relasi dengan Role
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private RoleModel role;
+
+    //Relasi dengan Produksi
+    @OneToMany(mappedBy = "idProduksi", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<ProduksiModel> listProduksi;
+
+    //Relasi dengan Delivery
+    @OneToMany(mappedBy = "idDelivery", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<DeliveryModel> listDelivery;
 }
--- sifactory/src/main/java/apap/TugasAkhir/siFactory/model/RoleModel.java
+++ sifactory/src/main/java/apap/TugasAkhir/siFactory/model/RoleModel.java
@@ -1,16 +1,9 @@
 package apap.TugasAkhir.siFactory.model;
 
-
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -37,5 +30,15 @@ public class RoleModel implements Serializable{
     @NotNull
     @Size(max = 50)
     @Column(name="role", nullable = false)
-    private String Role;
+    private String role;
+
+    @NotNull
+    @Column(name = "base_wages", nullable = false)
+    private Integer baseWages;
+
+    // Relationship dengan RoleModel
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private List<PegawaiModel> pegawaiRole;
 }

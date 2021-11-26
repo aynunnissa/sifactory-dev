64bbd64beb234bc67a47b45d42eeb05e2c56ba1c
maulanawildan <m.wildan.11@gmail.com>
2021-11-26 16:21:30 +0700

    feat: add PageController, add resources folder[ static(css & js), templates(fragments & home.html)]

---
+++ sifactory/src/main/java/apap/TugasAkhir/siFactory/controller/PageController.java
@@ -0,0 +1,15 @@
+package apap.TugasAkhir.siFactory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+//    @RequestMapping("/login")
+//    public String login(){ return "login"; }
+}

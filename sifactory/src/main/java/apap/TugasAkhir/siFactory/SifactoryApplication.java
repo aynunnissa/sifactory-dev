77a6275e6e49d8c2232fa8246cb29822bcd99af1
Aynun Nissa Setiawan <aynun.nissa@ui.ac.id>
2021-12-09 16:51:00 +0700

    chore: add swagger

--- sifactory/src/main/java/apap/TugasAkhir/siFactory/SifactoryApplication.java
+++ sifactory/src/main/java/apap/TugasAkhir/siFactory/SifactoryApplication.java
@@ -3,6 +3,13 @@ package apap.TugasAkhir.siFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL")
+})
+
 @SpringBootApplication
 public class SifactoryApplication {
 
